888cf01cf47777f2a31546f11aeb9b42a3512fea
Sandra Kao <63223781+Sandra-Kao@users.noreply.github.com>
2021-04-19 18:18:17 +0800

    Create JavaScript-weird-Boolean-conversion.js

---
+++ JavaScript-weird-Boolean-conversion.js
@@ -0,0 +1,17 @@
+console.clear();
+//...//
+
+
+let a = Boolean(0);
+console.log(!!a);
+
+let b = new Boolean(0);
+console.log(!!b);
+
+
+// console.log(!!Boolean(0));
+// console.log(!!new Boolean(0));
+
+
+//...//
+//Because the new Boolean(); always return 'true'
